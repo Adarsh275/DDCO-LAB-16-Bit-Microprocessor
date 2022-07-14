dae61692d1560577b2b914800e87f79b1d85747f
Adarsh275 <adarshaman2000@gmail.com>
2022-07-14 08:50:56 +0530

    Week_1 basic gates and adder

---
+++ week1/week1.2/full_adder.v
@@ -0,0 +1,6 @@
+//fulladder 
+module fulladd (input wire a, b, cin, output wire sum, cout);
+assign sum = (a ^ b) ^ cin;
+assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
+
---
+++ week1/week1.2/half_add.v
@@ -0,0 +1,6 @@
+// halfadder 
+module halfadd (input wire a, b, output wire s, cout);
+assign s = (a ^ b);
+assign cout = (a & b);
+endmodule
+
